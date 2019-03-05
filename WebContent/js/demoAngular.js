68400e4f3cbc347b97bffa66897dbfbe8bc1080f
daniel050697 <danielsantos06@yahoo.it>
2019-03-05 11:42:50 +0100

    Test v1

--- WebContent/js/demoAngular.js
+++ WebContent/js/demoAngular.js
@@ -2,6 +2,11 @@ var app = angular.module("myapp",[]);
 
 app.controller("ctrl", function($scope){
 	
-	$scope.customers =[{name: 'Daniel Santos', city: 'Milan' },{name: 'L'}]
+	$scope.customers =[
+		{name: 'Daniel Santos', city: 'Milan' },
+		{name: 'Lorenzo Nosotti', city: 'Milan'},
+		{name: 'Marco Chiappini', city: 'Milan'},
+		{name: 'Mario', city: 'Napoli'}
+		];
 	
 })
